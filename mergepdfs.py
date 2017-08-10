7a0c65e34062d30cc5a3a0d9516cb1be18498bc4
Unknown <remi.lavedrine@outlook.com>
2017-08-10 10:36:18 +0200

    Change traversal/merger algorithm
    I am relying on a while loop instead of a for loop as it allows us to move directly to one part of the list as I treated the previous files.
    I "Close()" the file merger when the appending phase is done.
    Removing the "string" import as it is not necessary.
    Removing previous algorithm that was commented at the moment.

--- mergepdfs.py
+++ mergepdfs.py
@@ -4,7 +4,6 @@ Hello
 #!/usr/bin/python3
 
 import os.path
-import string
 import parsepdfsfile
 
 
@@ -19,32 +18,22 @@ def merge_pdf_files(file_names_list):
 	# Create a PdfFileMerger object.
     from PyPDF2 import PdfFileMerger
     first_file_index = 0
-    counter = 1
+    counter = 0
 
     # Go through all elements of the list.
-    for first_file_index in range(0, len(file_names_list)-1):
+    while first_file_index < len(file_names_list):
         merger = PdfFileMerger()
-        while file_names_list[first_file_index].split(" - part", 2)[0] == file_names_list[first_file_index + counter].split(" - part", 2)[0]:
-            merger.append(os.path.join(PATH, file_names_list[first_file_index]), 'rb')
+        # Before appending the file, be sure that you are not testing above the list last item.
+        while first_file_index + counter <len(file_names_list) and file_names_list[first_file_index].split(" - part", 2)[0] == file_names_list[first_file_index + counter].split(" - part", 2)[0]:
+            merger.append(os.path.join(PATH, file_names_list[first_file_index + counter]), 'rb')
             counter = counter + 1
 
         # Write the last files' appends to a new file
         merger.write(os.path.join(PATH, "results", file_names_list[first_file_index].split(" - part", 2)[0] + ".pdf"))
-        #merger.close()
+        merger.close()
+        # Add the counter to the first_file_counter to go to the next file.
         first_file_index = first_file_index + counter
         counter = 0
-
-    ''' for file_name in file_names_list:
-        if file_name != previous_file_name:
-            file_name_without_part = file_name.split(" - part", 2)
-            if previous_file_name == "" or previous_file_name.startswith(file_name_without_part[0]):
-                merger.append(os.path.join(PATH, file_name), 'rb')
-            else:
-                merger.append(os.path.join(PATH, file_name), 'rb')
-
-                # Write all the appends to a new file
-                merger.write(os.path.join(PATH, previous_file_name.split(" - part", 2)[0] + ".pdf"))
-            previous_file_name = file_name '''
 
 
 def main():
