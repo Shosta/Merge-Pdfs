6a18a37fad6b78ef35b2634278f3237117fef462
Rem's <remi.lavedrine@outlook.com>
2017-08-15 10:45:09 +0200

    Add default value for separator and source folder
    I am adding the default value of the source_folder to '.' whatever and the default value of the separator to  ' - part' whatever.
    Change name of target_folder to source_folder.
    I am adding precision to the command usage.

--- mergepdfs.py
+++ mergepdfs.py
@@ -13,51 +13,49 @@ def usage():
     '''
     Display the awaited command and arguments for the 'mergepdfs.py'.
     '''
-    print('mergepdfs.py -t <target_folder> -s <separator>')
-    print('Default folder is the current folder.')
-    print('Default separator is \' - part\'.')
+    print('Here is the default usage of that command:')
+    print('mergepdfs.py -f <source_folder> -s <separator>')
+    print('We don\'t detect any folder and so we are selecting the default folder is the '
+          'current folder.')
+    print('We don\'t detect any folder and so we are selecting the default separator is '
+          '\' - part\'.')
 
 
 def main(argv):
     '''Main function'''
-    target_folder = ''
-    separator = ''
+    source_folder = '.'
+    separator = ' - part'
     try:
-       opts, args = getopt.getopt(argv,"ht:s:",["help=", "target_folder=","separator="])
+        opts, args = getopt.getopt(argv, "hf:s:", ["help=", "source_folder=", "separator="])
     except getopt.GetoptError:
         print('Type \'mergepdfs.py -h\' for help.')
         sys.exit(2)
 
     if opts.__len__() == 0:
         usage()
-        target_folder = '.'
-        separator = ' - part'
 
     for opt, arg in opts:
         if opt == '-h':
             usage()
             sys.exit()
-        elif opt in ("-t", "--target_folder"):
-            target_folder = arg
+        elif opt in ("-f", "--source_folder"):
+            source_folder = arg
         elif opt in ("-s", "--separator"):
-            if arg != '':
-                separator = arg
-            else:
-                print('Default separator')
-                separator = ' - part'
-    print('Target Folder is: \"' + target_folder + "\"")
+            separator = arg
+    print('Target Folder is: \"' + source_folder + "\"")
     print('Separator is: \"' + separator + "\"")
 
 
     # Parse the files.
-    file_names_list = parsepdfsfile.parse_file(target_folder)
+    file_names_list = parsepdfsfile.parse_file(source_folder)
     file_names_list = parsepdfsfile.remove_non_pdf_files(file_names_list)
 
     # Merge the files.
-    mergefiles.create_default_results_folder(target_folder)
-    mergefiles.merge_pdf_files(target_folder, file_names_list, separator)
+    mergefiles.create_default_results_folder(source_folder)
+    mergefiles.merge_pdf_files(source_folder, file_names_list, separator)
 
     print('Files merged successfully')
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
