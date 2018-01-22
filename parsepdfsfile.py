0e562d0c2b19aebf0fb0835265372c4a8573335a
Unknown <remi.lavedrine@outlook.com>
2018-01-22 14:48:34 +0100

    :Bug: Sort the files in a natural way that humans expect.
    String sort method in Python is done as followed
    1, 2, 10, 12 returns 1, 10, 12, 2
    which is not what humans expect by sorting list like this.
    Changed the sorting algortihm so that it sorts as followed : 
    1, 10, 12, 2 returns 1, 2, 10, 12

--- parsepdfsfile.py
+++ parsepdfsfile.py
@@ -2,6 +2,16 @@
 Parse the files in a directory and return a sorted array of Pdf files in that directory.
 '''
 
+import re
+def natural_sort(list_to_sort):
+    '''
+    Sort the given list in the way that humans expect.
+    '''
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    list_to_sort.sort(key=alphanum_key)
+
+
 def parse_file(dir_path):
     '''
     Parse a directory and return an array that contains all the files in it.
@@ -15,7 +25,7 @@ def parse_file(dir_path):
     from os import listdir
     from os.path import isfile, join
     only_files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
-    only_files.sort()
+    natural_sort(only_files)
 
     return only_files
 
